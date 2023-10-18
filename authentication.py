c9491455c7c21e421095d83989c22f11d7ec289f
Eric <diego123@Eric.local>
2023-10-18 17:46:19 -0600

    format code

--- authentication.py
+++ authentication.py
@@ -1,6 +1,27 @@
+from fastapi.exceptions import HTTPException
+from fastapi import status
 from passlib.context import CryptContext
+from dotenv import dotenv_values
+import jwt
+from models import User
 
+config_credentials = dotenv_values(".env")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def get_hashed_password(password: str):
     return pwd_context.hash(password)
+
+
+async def very_token(token: str):
+    try:
+        payload = jwt.decode(token, config_credentials["SECRET"], algorithms=["HS256"])
+        user = await User.get(id=payload["id"])
+
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
---
+++ emails.py
@@ -0,0 +1,73 @@
+import jwt
+from fastapi import (
+    BackgroundTasks,
+    UploadFile,
+    File,
+    Form,
+    Depends,
+    HTTPException,
+    status,
+)
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from pydantic import BaseModel, EmailStr
+from typing import List
+
+from dotenv import dotenv_values
+from models import User
+
+config_credentials = dotenv_values(".env")
+print(">>>>>>email", config_credentials["EMAIL"])
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=config_credentials["EMAIL"],
+    MAIL_PASSWORD=config_credentials["PASS"],
+    MAIL_FROM=config_credentials["EMAIL"],
+    MAIL_PORT=465,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+)
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+
+async def send_email(email: List, instance: User):
+    token_data = {"id": instance.id, "username": instance.user_name}
+
+    token = jwt.encode(token_data, config_credentials["SECRET"], algorithm="HS256")
+
+    template = f"""
+        <!DOCTYPE html>
+        <html>
+            <head>
+            <body>
+                <div style = "display: flex; align-items: center; justify-content: center flex-direction: column">
+                    <h3>Account Verification</h3>
+                    <br>
+                    <p>Thank you for registering with us, please click on the link below to verify your account</p>
+                    <a style="margin-top:1rem; padding: 1rem; border-radius: 0.5rem;
+                    background-color: #0275d8; text-decoration: none font-size: 1rem; color: white;"
+                    href="http://localhost:8000/verification/?token={token}">
+                        Verify your email
+                    </a>
+                    <p> please kindly  ignore this email if you did not register </p>
+                </div>
+            </body>
+            </head>
+        </html>
+    
+    """
+
+    message = MessageSchema(
+        subject="Account Verification",
+        recipients=email,
+        body=template,
+        subtype="html",
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message=message)
